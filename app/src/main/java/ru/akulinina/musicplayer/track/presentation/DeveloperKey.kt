6ff0a913eca0cd14ea8a62457eaa5041b0d4776a
HelgaPolis <olya.bazalikova@gmail.com>
2023-01-27 21:47:48 +0700

    file is removed

--- app/src/main/java/ru/akulinina/musicplayer/track/presentation/DeveloperKey.kt
+++
@@ -1,8 +0,0 @@
-package ru.akulinina.musicplayer.track.presentation
-
-class DeveloperKey {
-
-    companion object {
-        const val DEVELOPER_KEY = "AIzaSyCKTuXO6N2OGAU9aP8ioRNVQwc2VtixhOg"
-    }
-}
